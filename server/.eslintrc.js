27472da15fc4c1ab06aa2d3a8b21a6dedb72878c
Manav Misra <manavm@visionwebsoft.com>
2023-03-06 12:07:11 -0600

    Integrate ESLint for GraphQL 🤞🏾

--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -4,7 +4,19 @@ module.exports = {
     node: true,
   },
   extends: ["standard", "prettier"],
-  overrides: [],
+  overrides: [
+    {
+      files: ["*.js"],
+      processor: "@graphql-eslint/graphql",
+    },
+    {
+      files: ["*.graphql"],
+      extends: [
+        "plugin:@graphql-eslint/schema-recommended",
+        "plugin:@graphql-eslint/operations-recommended",
+      ],
+    },
+  ],
   parserOptions: {
     ecmaVersion: "latest",
     sourceType: "module",
