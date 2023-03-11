d0817fb04367c85c8a7d52913b3e623e37045e3a
Manav Misra <manavm@visionwebsoft.com>
2023-03-11 14:28:24 -0600

    Set  up mocks to return 9 🐱s with uuids

--- server/src/graphql/mocks.js
+++ server/src/graphql/mocks.js
@@ -1,34 +1,38 @@
+import { v4 as uuidv4 } from "uuid";
+
 const mocks = {
   Query() {
     return {
-      indexTracks() {
-        return [...new Array(6)];
+      tracks() {
+        return [...new Array(9)];
       },
     };
   },
-  Track: {
-    id() {
-      return "track_01";
-    },
-    title() {
-      return "Astro Kitty, Space Explorer";
-    },
-    author() {
-      return {
-        name: "Grumpy Cat",
-        photo:
-          "https://res.cloudinary.com/dety84pbu/image/upload/v1606816219/kitty-veyron-sm_mctf3c.jpg",
-      };
-    },
-    thumbnail() {
-      return "https://res.cloudinary.com/dety84pbu/image/upload/v1598465568/nebula_cat_djkt9r.jpg";
-    },
-    length() {
-      return 1210;
-    },
-    modulesCount() {
-      return 6;
-    },
+  Track() {
+    return {
+      id() {
+        return uuidv4();
+      },
+      title() {
+        return "Astro Kitty, Space Explorer";
+      },
+      author() {
+        return {
+          name: "Grumpy Cat",
+          photo:
+            "https://res.cloudinary.com/dety84pbu/image/upload/v1606816219/kitty-veyron-sm_mctf3c.jpg",
+        };
+      },
+      thumbnail() {
+        return "https://res.cloudinary.com/dety84pbu/image/upload/v1598465568/nebula_cat_djkt9r.jpg";
+      },
+      length() {
+        return 1210;
+      },
+      modulesCount() {
+        return 6;
+      },
+    };
   },
 };
 
