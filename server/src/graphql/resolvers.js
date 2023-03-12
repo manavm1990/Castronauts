0d0f4090c2578e9171b45d27cd4a46ea0c7422ec
Manav Misra <manavm@visionwebsoft.com>
2023-03-11 19:31:15 -0600

    docs: :bulb: add comments about resolver ⛓️

--- server/src/graphql/resolvers.js
+++ server/src/graphql/resolvers.js
@@ -7,6 +7,10 @@ export default {
       return dataSources.trackAPI.showTrack(id);
     },
   },
+
+  // The resolver for the Track type's modules field
+  // Keeping it separated means it only gets called when the modules field is requested
+  // (parent is the track) - RESOLVER ⛓️
   Track: {
     // The resolver for the author field on the Track type (parent is the track)
     author({ authorId }, _, { dataSources }) {
