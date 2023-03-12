a6cd88247496e964ab57e5b7801637c30042bf3e
Manav Misra <manavm@visionwebsoft.com>
2023-03-11 19:26:55 -0600

    Add track resolver

--- server/src/graphql/resolvers.js
+++ server/src/graphql/resolvers.js
@@ -3,6 +3,9 @@ export default {
     tracks(_, __, { dataSources }) {
       return dataSources.trackAPI.indexTracks();
     },
+    track(_, { id }, { dataSources }) {
+      return dataSources.trackAPI.showTrack(id);
+    },
   },
   Track: {
     // The resolver for the author field on the Track type (parent is the track)
