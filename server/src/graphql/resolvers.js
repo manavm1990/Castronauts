45795325238d5e210e6532c785cc4b0d65b2b087
Manav Misra <manavm@visionwebsoft.com>
2023-03-11 19:37:08 -0600

    Add to resolver chain for Track modules
    Includes heavy commenting 💡.

--- server/src/graphql/resolvers.js
+++ server/src/graphql/resolvers.js
@@ -1,3 +1,13 @@
+/**
+  * A resolver function populates the data for a field in your schema.
+  *  The function has four parameters. 
+  * The first, parent, contains the returned data of the previous function in the 
+resolver chain.
+  * The second, args, is an object that contains all the arguments provided to the field. 
+  * We use the third parameter, context, to access data sources such as a database or REST API. 
+  * Finally, the fourth parameter, info, contains informational properties about the operation state.
+*/
+
 export default {
   Query: {
     tracks(_, __, { dataSources }) {
@@ -15,6 +25,10 @@ export default {
     // The resolver for the author field on the Track type (parent is the track)
     author({ authorId }, _, { dataSources }) {
       return dataSources.trackAPI.showAuthor(authorId);
+    },
+    // The resolver for the modules field on the Track type (parent is the track)
+    modules({ id }, _, { dataSources }) {
+      return dataSources.trackAPI.getTrackModules(id);
     },
   },
 };
