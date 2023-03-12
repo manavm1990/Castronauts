6092e7e626a9302208271dea474095e9ff2f5b9b
Manav Misra <manavm@visionwebsoft.com>
2023-03-11 21:37:29 -0600

    Let Query use Container

--- client/src/components/query.jsx
+++ client/src/components/query.jsx
@@ -1,25 +1,27 @@
 import { useQuery } from "@apollo/client";
 import PropTypes from "prop-types";
 import Alert from "react-bootstrap/Alert";
+import Container from "react-bootstrap/Container";
 import Spinner from "react-bootstrap/Spinner";
 
 export default function Query({ typeDef, children }) {
   const { loading, error, data } = useQuery(typeDef);
 
-  if (loading)
-    return (
-      <Spinner animation="border" variant="success">
-        <p className="visually-hidden">Loading...</p>
-      </Spinner>
-    );
+  const render = () => {
+    if (loading) return <Spinner animation="border" variant="success" />;
+    if (error) return <Alert variant="danger">❗ Error :( 🥅</Alert>;
+    if (data && data.length)
+      return <Alert variant="warning">⚠️ No data :(</Alert>;
 
-  if (error) return <Alert variant="danger">❗ Error :( 🥅</Alert>;
+    // RENDER PROPS - a component receives a function as a prop and uses that function to render content.
+    return children(data);
+  };
 
-  if (data && data.length)
-    return <Alert variant="warning">⚠️ No data :(</Alert>;
-
-  // RENDER PROPS - a component receives a function as a prop and uses that function to render content.
-  return children(data);
+  return (
+    <Container as="main" className="bg-light py-4">
+      {render()}
+    </Container>
+  );
 }
 
 Query.propTypes = {
--- client/src/routes/home.jsx
+++ client/src/routes/home.jsx
@@ -1,5 +1,4 @@
 import Col from "react-bootstrap/Col";
-import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Query from "../components/query";
 import TrackCard from "../components/track-card";
@@ -7,18 +6,16 @@ import { TRACKS } from "../graphql/type-defs";
 
 export default function Main() {
   return (
-    <Container as="main" className="bg-light py-4">
-      <Query typeDef={TRACKS}>
-        {({ tracks }) => (
-          <Row>
-            {tracks.map((track) => (
-              <Col key={track.id} md={4} className="my-2">
-                <TrackCard track={track} />
-              </Col>
-            ))}
-          </Row>
-        )}
-      </Query>
-    </Container>
+    <Query typeDef={TRACKS}>
+      {({ tracks }) => (
+        <Row>
+          {tracks.map((track) => (
+            <Col key={track.id} md={4} className="my-2">
+              <TrackCard track={track} />
+            </Col>
+          ))}
+        </Row>
+      )}
+    </Query>
   );
 }
