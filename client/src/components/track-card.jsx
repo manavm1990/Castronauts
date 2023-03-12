8dccf362f366d5b5c97581c211cbf89c5859d907
Manav Misra <manavm@visionwebsoft.com>
2023-03-12 12:50:46 -0500

    feat: link to individual Card

--- client/src/components/track-card.jsx
+++ client/src/components/track-card.jsx
@@ -1,43 +1,47 @@
 import PropTypes from "prop-types";
 import Card from "react-bootstrap/Card";
+import { Link } from "react-router-dom";
 
 export default function TrackCard({ track }) {
   return (
-    <Card>
-      <Card.Img
-        variant="top"
-        src={track.thumbnail}
-        style={{
-          filter: "grayscale(60%)",
-        }}
-      />
-      <Card.Body as="section">
-        <Card.Title as="h2" className="mb-5 text-center text-dark">
-          {track.title}
-        </Card.Title>
-        <div className="d-flex">
-          <img
-            src={track.author.photo}
-            alt=""
-            width={32}
-            height={32}
-            className="rounded-circle me-2"
-          />
-          <div className="d-flex flex-column justify-content-center lh-sm">
-            <Card.Text
-              className="fw-semibold mb-0 text-secondary"
-              style={{ fontSize: "0.8rem" }}
-            >
-              {track.author.name}
-            </Card.Text>
-            <p className=" mb-0">
-              <Card.Text as="small">{track.length}</Card.Text>&nbsp;&bull;&nbsp;
-              <Card.Text as="small">{track.modulesCount}</Card.Text>
-            </p>
+    <Link to={`/${track.id}`}>
+      <Card>
+        <Card.Img
+          variant="top"
+          src={track.thumbnail}
+          style={{
+            filter: "grayscale(60%)",
+          }}
+        />
+        <Card.Body as="section">
+          <Card.Title as="h2" className="mb-5 text-center text-dark">
+            {track.title}
+          </Card.Title>
+          <div className="d-flex">
+            <img
+              src={track.author.photo}
+              alt=""
+              width={32}
+              height={32}
+              className="rounded-circle me-2"
+            />
+            <div className="d-flex flex-column justify-content-center lh-sm">
+              <Card.Text
+                className="fw-semibold mb-0 text-secondary"
+                style={{ fontSize: "0.8rem" }}
+              >
+                {track.author.name}
+              </Card.Text>
+              <p className=" mb-0">
+                <Card.Text as="small">{track.length}</Card.Text>
+                &nbsp;&bull;&nbsp;
+                <Card.Text as="small">{track.modulesCount}</Card.Text>
+              </p>
+            </div>
           </div>
-        </div>
-      </Card.Body>
-    </Card>
+        </Card.Body>
+      </Card>
+    </Link>
   );
 }
 
